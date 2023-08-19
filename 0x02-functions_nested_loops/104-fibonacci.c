9234c8e8f3030d6462236fb32ec39cbef30e08ac
sophiabassey1912 <sophiabassey01912@gmail.com>
2023-08-19 07:31:02 -0700

    task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,42 +1,45 @@
 #include <stdio.h>
 /**
-  * main - print a table of the first 178 fibonacci numbers.
+  * main - print the first 98 fibonacci numbers.
   * Return: Nothing.
   */
 int main(void)
 {
 	int count;
 	unsigned long i, j, k;
-	unsigned long ia, ib, ja, jb, ka, kb, carry;
-	unsigned long amount;
+	unsigned long m, n, p, carry;
 
 	count = 0;
 	i = 0;
 	j = 1;
-	amount = 10000000000000000000;/*10^18*/
 	for (count = 1; count <= 91; count++)
 	{
 		k = i + j;
 		i = j;
 		j = k;
-		printf("%d %lu\n", count + 1, k);
+		printf("%lu, ", k);
 	}
-	ia = i / amount;
-	ib = i % amount;
-	ja = j / amount;
-	jb = j % amount;
-	while (count <= 178)
+	m = i % 1000;
+	i = i / 1000;
+	n = j % 1000;
+	j = j / 1000;
+	while (count <= 98)
 	{
-		carry = (ib + jb) / amount;
-		ka = (ia + ja) + carry;
-		kb = (ib + jb ) - carry * amount;
-		ia = ja;
-		ib = jb;
-		ja = ka;
-		jb = kb;
-		printf("%d %lu%018lu\n", count + 1, ka, kb);
+		carry = (m + n) / 1000;
+		p = (m + n) - carry * 1000;
+		k = (i + j) + carry;
+		m = n;
+		n = p;
+		i = j;
+		j = k;
+		if (p >= 100)
+			printf("%lu%lu", k, p);
+		else
+			printf("%lu0%lu", k, p);
+		if (count != 98)
+			printf(", ");
 		count++;
 	}
-	printf("%lu018", ka);
+	putchar('\n');
 	return (0);
 }
